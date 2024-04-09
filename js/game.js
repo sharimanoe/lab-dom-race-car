a7bb91ab9e1c0923214986263ef46e9faffe770d
Noelia <sharimanoe@gmail.com>
2024-04-09 14:05:58 +0200

    lab resolution

--- js/game.js
+++ js/game.js
@@ -1,3 +1,44 @@
 class Game {
-    // code to be added
+  constructor() {
+    this.startScreen = document.getElementById("game-intro");
+    this.gameScreen = document.getElementById("game-screen");
+    this.gameEndScreen = document.getElementById("game-end");
+    this.player = new Player(
+      this.gameScreen,
+      200,
+      500,
+      100,
+      150,
+      "/images/car.png"
+    );
+    this.height = 600;
+    this.width = 500;
+    this.obstacles = [];
+    this.score = 0;
+    this.lives = 3;
+    this.gameIsOver = false;
+    this.gameIntervalId;
+    this.gameLoopFrecuency = Math.round(1000 / 60); //60fps
+  }
+
+  start() {
+    this.gameScreen.style.height = `${this.height}px`;
+    this.gameScreen.style.width = `${this.width}px`;
+
+    this.startScreen.style.display = "none";
+    this.gameScreen.style.display = "block";
+
+    this.gameIntervalId = setInterval(() => {
+      this.gameLoop();
+    }, this.gameLoopFrecuency);
+  }
+
+  gameLoop() {
+    console.log("gameLoop method");
+    this.update();
+
+    if (this.gameIsOver) {
+      clearInterval(this.gameIntervalId);
+    }
+  }
 }
---
+++ js/obstacle.js
@@ -0,0 +1,31 @@
+class Obstacle {
+  constructor(gameScreen) {
+    this.gameScreen = gameScreen;
+    this.left = Math.floor(Math.random() * 300 + 70);
+    this.top = 0;
+    this.width = 100;
+    this.height = 150;
+    this.element = document.createElement("img");
+
+    this.element.src = "/images/redCar.png";
+    this.element.style.position = "absolute";
+    this.element.style.width = `${width}px`;
+    this.element.style.heigh = `${height}px`;
+    this.element.style.left = `${left}px`;
+    this.element.style.top = `${top}px`;
+
+    this.gameScreen.appendChild(this.element);
+  }
+  updatePosition() {
+    // Update the obstacle's position based on the properties left and top
+    this.element.style.left = `${this.left}px`;
+    this.element.style.top = `${this.top}px`;
+  }
+
+  move() {
+    // Move the obstacle down by 3px
+    this.top += 3;
+    // Update the obstacle's position on the screen
+    this.updatePosition();
+  }
+}
